014622180dc4073aebebb74c6353c97ac4f59833
Ubuntu <ubuntu@ip-172-31-7-143.us-west-1.compute.internal>
2016-11-03 18:59:38 +0000

    Test Docker App

---
+++ DockerTestApp/testapp/config/settings.py
@@ -0,0 +1,9 @@
+SECRET_KEY = 'keepitasecret'
+
+APP_NAME = 'Test Docker App'
+
+DEBUG = True
+TESTING = False
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://mobydock:yourpassword@localhost:5432/testapp'
+REDIS_URL = 'redis://localhost:6379/0'
